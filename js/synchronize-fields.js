74375aaa6c67882d5e96da7450b10e7ff12c1f20
eteries <internet.practica@gmail.com>
2017-02-15 23:59:23 +0300

    module6-task1 (fix)

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,10 +2,7 @@
 
 window.synchronizeFields = (function () {
   return function (elementFrom, elementTo, arrayFrom, arrayTo, attr) {
-
-    return function () {
-      var selectedIndex = arrayFrom.indexOf(elementFrom.value);
-      elementTo[attr] = arrayTo[selectedIndex];
-    };
+    var selectedIndex = arrayFrom.indexOf(elementFrom.value);
+    elementTo[attr] = arrayTo[selectedIndex];
   };
 })();
